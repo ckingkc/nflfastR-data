8b22b3e7a4fff9091522a94758fe864ede6ca15e
Tan-DESKPC <tan@tanho.ca>
2021-08-22 16:51:48 -0400

    nuke and restart

---
+++ R/update_current_season_pbp.R
@@ -0,0 +1,10 @@
+source("R/save_pbp.R")
+
+y <- dplyr::if_else(
+  lubridate::month(lubridate::today("America/New_York")) >= 9,
+  lubridate::year(lubridate::today("America/New_York")) ,
+  lubridate::year(lubridate::today("America/New_York")) - 1
+)
+
+future::plan("multisession")
+save_pbp(y)
