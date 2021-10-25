689ca1b3c7775f0c75c2e18a6aec150f58c8c4a8
Tan Ho <38083823+tanho63@users.noreply.github.com>
2021-10-25 11:24:12 -0400

    skip to using Lee's actual schedules file

--- R/save_pbp.R
+++ R/save_pbp.R
@@ -1,8 +1,8 @@
 library(dplyr)
 save_pbp <- function(season) {
 
-  ids <- nflfastR::fast_scraper_schedules(season) %>%
-    dplyr::filter(!is.na(home_result)) %>%
+  ids <- nflreadr::load_schedules(season) |>
+    dplyr::filter(!is.na(result)) |>
     dplyr::pull(game_id)
 
   pbp <- nflfastR::build_nflfastR_pbp(ids)
